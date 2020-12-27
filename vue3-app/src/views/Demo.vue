389abfa758eadd2e48cc8a5a8450f2fec0cc022b
Dave Mount <davemount@gmail.com>
2020-12-26 22:14:58 -0800

    Settling down the config

--- vue3-app/src/views/Demo.vue
+++ vue3-app/src/views/Demo.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <h2>Demo of Components for Power Up</h2>
+    <form>
+      <label for="screenName">Screen Name</label>
+      <input type="text" name="screenName" />
+      <button>Push Me</button>
+    </form>
   </div>
 </template>
+<script>
+export default {}
+</script>
+<style>
+label {
+  font-weight: bold;
+}
+</style>
