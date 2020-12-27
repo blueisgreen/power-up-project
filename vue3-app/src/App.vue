913363e95fcc94e17310a5658da3c6b006f85106
Dave Mount <davemount@gmail.com>
2020-12-27 15:55:50 -0800

    Create base form fields

--- vue3-app/src/App.vue
+++ vue3-app/src/App.vue
@@ -12,7 +12,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -27,5 +26,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+form {
+  border: 4px groove #0080ff;
+  margin: 1em;
+  padding: 1em;
 }
 </style>
--- vue3-app/src/views/Demo.vue
+++ vue3-app/src/views/Demo.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="demo">
-    <h2>Demo of Components for Power Up</h2>
+    <h2>Demo of Components for Power Up!</h2>
     <form>
-      <label for="screenName">Screen Name</label>
-      <input type="text" name="screenName" />
-      <button>Push Me</button>
+      <h3>Member Info</h3>
+      <BaseInput v-model="member.screenName" label="Screen Name" type="text" />
+      <BaseInput v-model="member.email" label="Email" type="text" />
+      <BaseSelect
+        v-model="member.favoriteGenIVReactor"
+        label="Fav Gen IV Reactor"
+        :options="reactorTypes"
+      />
+      <button class="button-right" type="submit">Push Me</button>
     </form>
+    <div>
+      {{ $data }}
+    </div>
   </div>
 </template>
+
 <script>
-export default {}
+import BaseInput from '../components/BaseInput'
+import BaseSelect from '../components/BaseSelect'
+
+export default {
+  components: {
+    BaseInput,
+    BaseSelect
+  },
+  data: function() {
+    return {
+      reactorTypes: ['VHTR', 'GFR', 'SFR', 'LFR', 'MSR', 'SCWR'],
+      member: {
+        screenName: 'Zanzibar',
+        email: 'zanzibar@happyspiritgames.com',
+        favoriteGenIVReactor: 'SCWR'
+      }
+    }
+  }
+}
 </script>
+
 <style>
 label {
+  margin-right: 1em;
   font-weight: bold;
+}
+.button-right {
+  margin-left: 1em;
+}
+button {
+  font-size: small;
+  font-weight: bold;
+  background-color: paleturquoise;
 }
 </style>
