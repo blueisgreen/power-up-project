15e69d982b35a35c8736256631077b49873cc62a
Dave Mount <davemount@gmail.com>
2020-12-27 19:03:10 -0800

    refactor

--- vue3-app/src/views/Demo.vue
+++ vue3-app/src/views/Demo.vue
@@ -1,69 +1,20 @@
 <template>
   <div class="demo">
     <h1 style="text-align:center">Demo of Components for Power Up!</h1>
-    <form>
-      <h2>Member Info</h2>
-      <BaseInput v-model="member.screenName" label="Screen Name" type="text" />
-      <BaseInput v-model="member.email" label="Email" type="text" size="60" />
-      <BaseSelect
-        v-model="member.favoriteGenIVReactor"
-        label="Fav Gen IV Reactor"
-        :options="reactorTypes"
-      />
-      <BaseCheckbox
-        v-model="member.isAgreedToTerms"
-        label="I agree to the terms of use."
-      />
-      <BaseCheckbox
-        v-model="member.isActiveSubscription"
-        label="Send me the weekly newsletter."
-      />
-
-      <h4>Avatar Color</h4>
-      <BaseRadio
-        v-model="member.avatarColor"
-        value="silver"
-        label="silver"
-        name="avatarColor"
-      />
-      <BaseRadio
-        v-model="member.avatarColor"
-        value="gold"
-        label="gold"
-        name="avatarColor"
-      />
-      <BaseRadio
-        v-model="member.avatarColor"
-        value="platinum"
-        label="platinum"
-        name="avatarColor"
-      />
-
-      <div class="input">
-        <button class="button-right" type="submit">Push Me</button>
-      </div>
-    </form>
-    <div>
-      {{ $data }}
-    </div>
+    <MemberForm />
   </div>
 </template>
 
 <script>
-import BaseCheckbox from '../components/BaseCheckbox.vue'
+import MemberForm from '../components/MemberForm'
+
 export default {
-  components: { BaseCheckbox },
+  components: {
+    MemberForm
+  },
   data: function() {
     return {
-      reactorTypes: ['VHTR', 'GFR', 'SFR', 'LFR', 'MSR', 'SCWR'],
-      member: {
-        screenName: 'Zanzibar',
-        email: 'zanzibar@happyspiritgames.com',
-        favoriteGenIVReactor: 'SCWR',
-        isAgreedToTerms: false,
-        isActiveSubscription: true,
-        avatarColor: 'silver'
-      }
+      reactorTypes: ['VHTR', 'GFR', 'SFR', 'LFR', 'MSR', 'SCWR']
     }
   }
 }
