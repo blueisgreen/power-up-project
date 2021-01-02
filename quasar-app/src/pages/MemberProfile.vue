d108ae772b52db84a751438289427c6df3f4d5ab
Dave Mount <davemount@gmail.com>
2021-01-02 09:32:43 -0800

    Brainstorm items for member profile

--- quasar-app/src/pages/MemberProfile.vue
+++ quasar-app/src/pages/MemberProfile.vue
@@ -1,5 +1,22 @@
 <template>
   <q-page class="flex flex-center">
-    <h1>Your Member Profile</h1>
+    <div><h1>Your Member Profile</h1>
+    <ul>
+      <li>UUID</li>
+      <li>Real Name</li>
+      <li>Screen Name</li>
+      <li>Avatar</li>
+      <li>Self Introduction</li>
+      <li>Email</li>
+      <li>Email Is Verified</li>
+      <li>Google ID</li>
+      <li>LinkedIn ID</li>
+      <li>Twitter ID</li>
+      <li>Joined On</li>
+      <li>Roles</li>
+      <li>Badges</li>
+      <li>History</li>
+    </ul>
+    </div>
   </q-page>
 </template>
