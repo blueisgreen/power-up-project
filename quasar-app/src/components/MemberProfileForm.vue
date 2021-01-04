f38de88c4b6695b6ec71d75a24b2815109a31b88
Dave Mount <davemount@gmail.com>
2021-01-03 21:36:12 -0800

    Create form with a field

---
+++ quasar-app/src/components/MemberProfileForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="text" label="Screen Name" outlined />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemberProfileForm',
+  data() {
+    return {
+      text: 'Zanzibar',
+    }
+  },
+}
+</script>
--- quasar-app/src/pages/MemberProfile.vue
+++ quasar-app/src/pages/MemberProfile.vue
@@ -1,22 +1,32 @@
 <template>
   <q-page class="flex flex-center">
-    <div><h1>Your Profile</h1>
-    <ul>
-      <li>UUID</li>
-      <li>Real Name</li>
-      <li>Screen Name</li>
-      <li>Avatar</li>
-      <li>Self Introduction</li>
-      <li>Email</li>
-      <li>Email Is Verified</li>
-      <li>Google ID</li>
-      <li>LinkedIn ID</li>
-      <li>Twitter ID</li>
-      <li>Joined On</li>
-      <li>Roles</li>
-      <li>Badges</li>
-      <li>History</li>
-    </ul>
+    <div>
+      <h1>Your Profile</h1>
+      <ul>
+        <li>UUID</li>
+        <li>Real Name</li>
+        <li>Screen Name</li>
+        <li>Avatar</li>
+        <li>Self Introduction</li>
+        <li>Email</li>
+        <li>Email Is Verified</li>
+        <li>Google ID</li>
+        <li>LinkedIn ID</li>
+        <li>Twitter ID</li>
+        <li>Joined On</li>
+        <li>Roles</li>
+        <li>Badges</li>
+        <li>History</li>
+      </ul>
     </div>
+    <member-profile-form />
   </q-page>
 </template>
+
+<script>
+import MemberProfileForm from 'components/MemberProfileForm.vue'
+
+export default {
+  components: { MemberProfileForm },
+}
+</script>
