ec327147620c29295764a6fb9e81f704e183aa8d
Dave Mount <davemount@gmail.com>
2021-04-24 11:21:16 -0700

    Remove previous incarnation of Quasar app (Vue 2)

--- powerup-im/src/router/routes.js
+++ powerup-im/src/router/routes.js
@@ -6,17 +6,25 @@ const routes = [
       { path: '', component: () => import('pages/Index.vue') },
       {
         path: 'draft',
-        component: () => import('pages/LessonDraftingTable.vue')
-      }
-    ]
+        component: () => import('pages/LessonDraftingTable.vue'),
+      },
+      {
+        path: 'admin',
+        component: () => import('pages/AdminMembers.vue'),
+      },
+      {
+        path: 'profile',
+        component: () => import('pages/MemberProfile.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }
+    component: () => import('pages/Error404.vue'),
+  },
 ]
 
 export default routes
