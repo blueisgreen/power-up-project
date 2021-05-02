d98ee6d2a1a774f4912c6549b50ecdef9996650e
Dave Mount <davemount@gmail.com>
2021-05-02 10:17:05 -0700

    Create standard component for text editing.
    This has all of the defaults for the wysiwyg editor. Includes props
    for starting content as well as a function for saves to data store
    (and presumably backend storage).

---
+++ powerup-im/src/components/StandardEditor.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>{{ docName }}</div>
+  <q-btn color="primary" icon="save" label="Save Content" />
+  <q-editor
+    v-model="editor"
+    max-height="25rem"
+    placeholder="--content goes here--"
+    paragraph-tag="p"
+    toolbar-push
+    dark
+    :toolbar="[
+      ['left', 'center', 'right', 'justify'],
+      ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
+      ['hr', 'link'],
+      [
+        {
+          label: $q.lang.editor.formatting,
+          icon: $q.iconSet.editor.formatting,
+          list: 'no-icons',
+          options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code'],
+        },
+        {
+          label: $q.lang.editor.fontSize,
+          icon: $q.iconSet.editor.fontSize,
+          fixedLabel: true,
+          fixedIcon: true,
+          list: 'no-icons',
+          options: [
+            'size-1',
+            'size-2',
+            'size-3',
+            'size-4',
+            'size-5',
+            'size-6',
+            'size-7',
+          ],
+        },
+        {
+          label: $q.lang.editor.defaultFont,
+          icon: $q.iconSet.editor.font,
+          fixedIcon: true,
+          list: 'no-icons',
+          options: [
+            'default_font',
+            'arial',
+            'arial_black',
+            'comic_sans',
+            'courier_new',
+            'impact',
+            'lucida_grande',
+            'times_new_roman',
+            'verdana',
+          ],
+        },
+        'removeFormat',
+      ],
+      ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+      ['undo', 'redo'],
+      ['viewsource'],
+    ]"
+    :fonts="{
+      arial: 'Arial',
+      arial_black: 'Arial Black',
+      comic_sans: 'Comic Sans MS',
+      courier_new: 'Courier New',
+      impact: 'Impact',
+      lucida_grande: 'Lucida Grande',
+      times_new_roman: 'Times New Roman',
+      verdana: 'Verdana',
+    }"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      default: '',
+    },
+    docName: {
+      type: String,
+      default: 'Your passage',
+    },
+    onSave: {
+      type: Function,
+      default: () => console.log('implement me'),
+    },
+  },
+  data() {
+    return {
+      editor: 'What say you?',
+    }
+  },
+}
+</script>
+
+<style></style>
--- powerup-im/src/pages/LessonDraftingTable.vue
+++ powerup-im/src/pages/LessonDraftingTable.vue
@@ -45,86 +45,15 @@
 
     <!-- html content part -->
     <div class="q-pa-md q-gutter-sm">
-      <q-btn color="primary" icon="save" label="Save Content" />
-      <q-editor
-        v-model="editor"
-        max-height="25rem"
-        placeholder="--content goes here--"
-        paragraph-tag="p"
-        toolbar-push
-        dark
-        :toolbar="[
-          ['left', 'center', 'right', 'justify'],
-          ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
-          ['hr', 'link'],
-          [
-            {
-              label: $q.lang.editor.formatting,
-              icon: $q.iconSet.editor.formatting,
-              list: 'no-icons',
-              options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'code'],
-            },
-            {
-              label: $q.lang.editor.fontSize,
-              icon: $q.iconSet.editor.fontSize,
-              fixedLabel: true,
-              fixedIcon: true,
-              list: 'no-icons',
-              options: [
-                'size-1',
-                'size-2',
-                'size-3',
-                'size-4',
-                'size-5',
-                'size-6',
-                'size-7',
-              ],
-            },
-            {
-              label: $q.lang.editor.defaultFont,
-              icon: $q.iconSet.editor.font,
-              fixedIcon: true,
-              list: 'no-icons',
-              options: [
-                'default_font',
-                'arial',
-                'arial_black',
-                'comic_sans',
-                'courier_new',
-                'impact',
-                'lucida_grande',
-                'times_new_roman',
-                'verdana',
-              ],
-            },
-            'removeFormat',
-          ],
-          ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
-          ['undo', 'redo'],
-          ['viewsource'],
-        ]"
-        :fonts="{
-          arial: 'Arial',
-          arial_black: 'Arial Black',
-          comic_sans: 'Comic Sans MS',
-          courier_new: 'Courier New',
-          impact: 'Impact',
-          lucida_grande: 'Lucida Grande',
-          times_new_roman: 'Times New Roman',
-          verdana: 'Verdana',
-        }"
-      />
-    </div>
+      <!-- text content part -->
+      <standard-editor :doc-name="lessonTitle" />
 
-    <!-- video content part -->
-    <div class="q-pa-md">
+      <!-- video content part -->
       <q-form>
         <q-input v-model="video.url" outlined label="Video URL" />
       </q-form>
-    </div>
 
-    <!-- image content part -->
-    <div class="q-pa-md">
+      <!-- image content part -->
       <q-form>
         <q-input v-model="image.url" outlined label="Image URL" />
         <q-input v-model="image.alt" outlined label="Alt text" />
@@ -149,7 +78,10 @@
 </template>
 
 <script>
+import StandardEditor from 'components/StandardEditor.vue'
+
 export default {
+  components: { StandardEditor },
   data() {
     return {
       editor: 'Let me tell you about <b>nuclear</b> power plants.',
