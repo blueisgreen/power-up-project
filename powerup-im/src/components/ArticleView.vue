d383c167e3a67d0cdf9cf078b5eefff1844cd100
Dave Mount <davemount@gmail.com>
2021-05-30 13:51:50 -0700

    New ArticleView component

---
+++ powerup-im/src/components/ArticleView.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-card
+    class="my-card text-white"
+    style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+  >
+    <q-card-section>
+      <div class="text-h6">{{ article.headline }}</div>
+      <div class="text-subtitle2">by {{ article.byline }}</div>
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      <span v-html="article.content" />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-card {
+  width: 75%;
+  max-height: 250px;
+  overflow-y: scroll;
+}
+</style>
--- powerup-im/src/pages/Articles.vue
+++ powerup-im/src/pages/Articles.vue
@@ -1,48 +1,16 @@
 <template>
   <q-page class="q-pa-md">
     <h1>Articles</h1>
-    <div v-for="article in articles"
-        :key="article.id" class="row q-gutter-md">
-      <span
-        @click="() => selectArticle(article.id)"
-      >
-        <strong>{{article.headline}}</strong>. Published on {{date.formatDate(article.publishedAt, 'MMM D, YYYY @ H:mm')}}
+    <div v-for="article in articles" :key="article.id" class="row q-gutter-md">
+      <span @click="() => selectArticle(article.id)">
+        <strong>{{ article.headline }}</strong>. Published on
+        {{ date.formatDate(article.publishedAt, 'MMM D, YYYY @ H:mm') }}
       </span>
     </div>
-    <hr/>
-    <div class="row items-start q-gutter-md">
-      <q-card
-        v-if="activeArticle"
-        class="my-card text-white"
-        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-      >
-        <q-card-section>
-          <div class="text-h6">{{ activeArticle.headline }}</div>
-          <div class="text-subtitle2">by {{ activeArticle.byline }}</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <span v-html="activeArticle.content" />
-        </q-card-section>
-      </q-card>
+    <hr />
+    <div v-if="activeArticle" class="row items-start q-gutter-md">
+      <article-view :article="activeArticle" />
     </div>
-    <!-- <div class="row items-start q-gutter-md">
-      <q-card
-        v-for="article in articles"
-        :key="article.id"
-        class="my-card text-white"
-        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
-      >
-        <q-card-section>
-          <div class="text-h6">{{ article.headline }}</div>
-          <div class="text-subtitle2">by {{ article.byline }}</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <span v-html="article.content" />
-        </q-card-section>
-      </q-card>
-    </div> -->
   </q-page>
 </template>
 
@@ -51,8 +19,10 @@ import { ref, onMounted } from 'vue'
 import { api } from '../boot/axios'
 import { useQuasar } from 'quasar'
 import { date } from 'quasar'
+import ArticleView from '../components/ArticleView.vue'
 
 export default {
+  components: { ArticleView },
   setup() {
     const $q = useQuasar()
     const articles = ref(null)
@@ -76,21 +46,15 @@ export default {
     return {
       articles,
       activeArticle,
-      date
+      date,
     }
   },
   methods: {
     selectArticle(id) {
-      this.activeArticle = this.articles.find(article => article.id === id)
-    }
-  }
+      this.activeArticle = this.articles.find((article) => article.id === id)
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped>
-.my-card {
-  width: 75%;
-  max-height: 250px;
-  overflow-y: scroll;
-}
-</style>
+<style lang="scss" scoped></style>
